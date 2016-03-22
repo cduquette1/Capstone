87f230b2194f6bc2121d420d08d0e9b36a5c13f7
cduquette1 <courtney.duquette.12@cnu.edu>
2016-03-22 11:25:05 -0400

    part one of the demo interface

---
+++ DemoInterface.java
@@ -0,0 +1,46 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+@SuppressWarnings("serial")
+public class DemoInterface extends JFrame{
+
+    public DemoInterface() {
+        super("Web Classifer");
+        setLayout(new BorderLayout());
+        
+        //add more information to the top maybe
+        
+        final DisplayPanel display = new DisplayPanel();
+        add(display, BorderLayout.CENTER);
+        
+        
+        JPanel button = new JPanel(new FlowLayout());
+        add(button, BorderLayout.SOUTH);
+        
+        JButton classify = new JButton("Classify");
+        classify.addActionListener(new ActionListener () {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                display.classify();
+            }
+        });
+        button.add(classify);
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 125);
+        setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new DemoInterface();
+
+    }
+
+}
